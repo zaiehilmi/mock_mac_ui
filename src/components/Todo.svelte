9f4ca239ff701837135e84b31bedcf510d1d6a10
zaiehilmi <zaiehilmi@gmail.com>
2024-05-01 18:34:03 +0800

    feat: ada pilihan untuk tunjuk komponen Todo

--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
     export let title: string = "TODO";
+    export let isShown: boolean = false;
 </script>
 
-<div class="bg-gradient-to-r from-violet-500 to-purple-500 rounded-lg p-6 m-8">
-    <p class="text-white font-semibold">Miss: {title}</p>
-</div>
+{#if isShown}
+    <div
+        class="bg-gradient-to-r from-violet-500 to-purple-500 rounded-lg p-6 m-8"
+    >
+        <p class="text-white font-semibold">Miss: {title}</p>
+    </div>
+{/if}
